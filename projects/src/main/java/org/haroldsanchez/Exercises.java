c055a6791739084c7c6e7b448e9625a299bc2543
Harold Sanchez <haroldjosano23@gmail.com>
2023-06-21 18:30:26 -0400

    second day, add two exersices

--- projects/src/main/java/org/haroldsanchez/Exercises.java
+++ projects/src/main/java/org/haroldsanchez/Exercises.java
@@ -11,4 +11,17 @@ public class Exercises {
         }
         return cont;
     }
+    public int numberShort(int[] args){
+
+        int result=args[0];
+        for (int i=0;i<args.length;i++){
+            result=(args[i]<=result)?args[i]:result;
+        }
+        return result;
+    }
+
+    public String createPhoneNumber(int[] number){
+
+        return "("+number[0]+number[1]+number[2]+")"+number[3]+number[4]+number[5]+"-"+number[6]+number[7]+number[8]+number[9];
+    }
 }
--- projects/src/test/java/haroldsanchez/unittest/ExerciseTest.java
+++ projects/src/test/java/haroldsanchez/unittest/ExerciseTest.java
@@ -10,21 +10,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("Exercises")
 @Tag("Unittest")
 public class ExerciseTest {
+    Exercises exercises=new Exercises();
   @Test
   @DisplayName("Counter the number of times the character appears in the string")
   public void testSomething() {
 
-      Exercises exercises=new Exercises();
       String text1="fizzbuzz";
       String text2="FIZZBUZZ";
       char valor='z';
       int expectedResult=4;
-      int actualResult1= exercises.charCount(text1,valor);
+      int actualResult= exercises.charCount(text1,valor);
 
       System.out.println("expectedResult: ".concat(String.valueOf(expectedResult)));
-      System.out.println("actualResult: ".concat(String.valueOf(actualResult1)));
-      assertEquals(expectedResult, actualResult1);
+      System.out.println("actualResult: ".concat(String.valueOf(actualResult)));
+      assertEquals(expectedResult, actualResult);
 
+  }
+  @Test
+    @DisplayName("find the smallest integer")
+    public void testNumberShort(){
+      //int[] number1={34, 15, 88, 2};
+      int[] number1={34, -345, -1, 100};
+
+      int expectedResult=-345;
+      int actualResult= exercises.numberShort(number1);
+
+      System.out.println("expectedResult: ".concat(String.valueOf(expectedResult)));
+      System.out.println("actualResult: ".concat(String.valueOf(actualResult)));
+      assertEquals(expectedResult,actualResult);
+  }
+  @Test
+    @DisplayName("Returns a string in the form of  phone number")
+    public void testCreatePhoneNumber(){
+      int[] number={1,2,3,4,5,6,7,8,9,0};
+
+      String expectedResult="(123)456-7890";
+      String actualResult= exercises.createPhoneNumber(number);
+
+      System.out.println("expectedResult: ".concat(expectedResult));
+      System.out.println("actualResult: ".concat(actualResult));
+      assertEquals(expectedResult,actualResult);
   }
 
 }
